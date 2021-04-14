49cdeca45a1a35412944a959ee124f059aef5958
mainara07 <assis.mainara@gmail.com>
2021-04-14 18:21:11 -0300

    feat: movimentos da cobrinha

--- js/script.js
+++ js/script.js
@@ -4,21 +4,45 @@ let box = 32;
 let snake = [];
 snake[0] = {
   x: 8 * box,
-  y: 8 * box
-}
+  y: 8 * box,
+}; //cobrinha montada com um array de objeto com uma unica posicao com dir. alto e baixo
+let direcao = "right";
 
 function criarBG() {
   context.fillStyle = "lightgreen"; //trabalha com o estilo
-  context.fillRect(0, 0, 16 * box, 16 * box); //trabalha com x e y + altura e largura 
-} //desenha e define uma cor 
+  context.fillRect(0, 0, 16 * box, 16 * box); //trabalha com x e y + altura e largura
+} //desenha e define uma cor
 
-
-function criarCobrinha () {
-  for  (i=0; i < snake.length; i++) {
+function criarCobrinha() {
+  for (i = 0; i < snake.length; i++) {
     context.fillStyle = "green";
-    context.fillRect.snake[i].s, snake[i].y, box, box
+    context.fillRect.snake[i].s, snake[i].y, box, box;
   }
 }
 
-criarBG();
-criarCobrinha()
+function iniciarJogo() {
+  criarBG();
+  criarCobrinha();
+  
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+  //definindo condições de direção para cobrinha
+  if(direcao=="right") snakeX += box
+  if(direcao =="left") snakeX -= box
+  if(direcao == "up") snakeY -= box
+  if(direcao =="down") snakeY += box
+
+  //criando e retirando "cabeça" da cobrinha para seguir o jogo
+  snake.pop();
+
+  //criando nova cabeça para cobrinha e ela seguir o jogo 
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead)
+}
+
+let jogo = setInterval(iniciarJogo, 100);
