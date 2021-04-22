228ffa45969374da6202e90172be3437cb5ea170
Mainara Assis <assis.mainara@gmail.com>
2021-04-22 09:00:06 -0300

    criar comida da cobrinha

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ snake[0] = {
   y: 8 * box,
 }; //cobrinha montada com um array de objeto com uma unica posicao com dir. alto e baixo
 let direcao = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
   context.fillStyle = "lightgreen"; //trabalha com o estilo
@@ -19,7 +23,10 @@ function criarCobrinha() {
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
-
+function criarComida () {
+  context.fillStyle = "purple"
+  context.fillRect(food.x, food.y, box, box)
+}
 // evento para definir o andamento da cobra durante o jogo
 document.addEventListener("keydown", update);
 
@@ -39,6 +46,7 @@ function iniciarJogo() {
 
   criarBG();
   criarCobrinha();
+  criarComida();
   
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
